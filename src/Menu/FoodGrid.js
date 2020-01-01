55405a3c61673c640e9c0e59e9c92fa8f7d7b377
dika aditia <aditiaaadicka@gmail.com>
2020-01-01 20:03:04 +0700

    add css transition on menu

--- src/Menu/FoodGrid.js
+++ src/Menu/FoodGrid.js
@@ -1,16 +1,16 @@
-import styled from "styled-components";
-import { Title } from "../Styles/title";
+import styled from 'styled-components';
+import { Title } from '../Styles/title';
 
 export const FoodGrid = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 20px;
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	gap: 20px;
 `;
 
 export const FoodLabel = styled.div`
-  position: absolute;
-  background-color: rgba(256, 256, 256, 0.8);
-  padding: 5px;
+	position: absolute;
+	background-color: rgba(256, 256, 256, 0.8);
+	padding: 5px;
 `;
 
 export const Food = styled(Title)`
@@ -22,9 +22,15 @@ export const Food = styled(Title)`
   background-size: cover;
   filter: contrast(75%);
   border-radius: 7px;
-  box-shadow: 0px 0px 10px 0px grey;
+  margin-top: 5px;
+  transition-property: box-shadow margin-top filter;
+  transition-duration: .1s;
+  box-shadow: 0px 0px 2px 0px grey;
   &:hover {
     cursor: pointer;
-    opacity: 0.7;
+    filter: contrast(100%);
+    margin-top: 0px;
+    margin-bottom: 5px;
+    box-shadow: 0px 5px 10px 0px grey;
   }
 `;
